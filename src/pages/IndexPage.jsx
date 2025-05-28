73754c37f7e232508fd0f109ca62849962072252
Leonor Feliciano <96401019+leotheweirdo05@users.noreply.github.com>
2025-05-28 16:05:49 +0100

    fix: corrected text in upper tabs

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -15,9 +15,9 @@ export default function IndexPage() {
           <section>
             <div className="container text-center">
               <div className="row">
-                <div className="col">Column</div>
+                <div className="col">Games</div>
                 <div className="col"></div>
-                <div className="col">Column</div>
+                <div className="col">Leaderboards</div>
               </div>
             </div>
           </section>
