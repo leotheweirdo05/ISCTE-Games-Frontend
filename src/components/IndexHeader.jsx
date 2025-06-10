0498d699e0cfbd47a5c5760daccdd4af1ed01b78
guilherme-catoja <gmcas3@iscte-iul.pt>
2025-06-10 17:11:54 +0100

    style: fixed the index header

--- src/components/IndexHeader.jsx
+++ src/components/IndexHeader.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import styles from './IndexHeader.module.css';
 
 export default function IndexHeader() {
   return (
-    <header style={{ backgroundColor: "#0d28c2" }} className="text-white py-3 px-4">
-      <div className="d-flex align-items-center justify-content-between">
-        <img src="/images/iscte-logo.png" alt="ISCTE Logo" height="40" />
-        <div className="text-end">
+    <header className="header text-white py-3 px-4">
+      <div className="header-content">
+        <img src="/images/iscte-logo-no-background.png" alt="ISCTE Logo" height="40" />
+        <div className="header-text">
           <div className="small">15 Março 2025</div>
           <h5 className="mb-0">O teu dia</h5>
         </div>
