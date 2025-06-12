2db9b3b58a284f797406a44017e52aa3e72414b6
guilherme-catoja <gmcas3@iscte-iul.pt>
2025-06-12 19:57:01 +0100

    fix: connected the pinball and memory games to the backend

--- src/pages/games/Game2048.jsx
+++ src/pages/games/Game2048.jsx
@@ -1,9 +1,38 @@
-// src/pages/Game2048.jsx
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Helmet } from 'react-helmet';
 import Header from '../../components/Header';
 
 const Game2048 = () => {
+  const iframeRef = useRef(null);
+
+  useEffect(() => {
+    const sendToken = () => {
+      const token = localStorage.getItem('token');
+      if (token && iframeRef.current && iframeRef.current.contentWindow) {
+        iframeRef.current.contentWindow.postMessage(
+          { type: 'SET_TOKEN', token },
+          'http://localhost'
+        );
+        console.log('Sent token to 2048 iframe:', token);
+      } else {
+        console.log('Token or 2048 iframe not ready:', { token, iframe: !!iframeRef.current });
+      }
+    };
+
+    const iframe = iframeRef.current;
+    if (iframe) {
+      iframe.addEventListener('load', sendToken);
+    }
+    const timer = setTimeout(sendToken, 1000);
+
+    return () => {
+      if (iframe) {
+        iframe.removeEventListener('load', sendToken);
+      }
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <>
       <Helmet>
@@ -11,9 +40,13 @@ const Game2048 = () => {
       </Helmet>
 
       <Header />
-      
+
       <div className="game-wrapper">
-        <iframe src="/games/2048/2048.html" title="2048 Game" />
+        <iframe
+          ref={iframeRef}
+          src="/games/2048/2048.worker.html"
+          title="2048 Game"
+        />
       </div>
     </>
   );
